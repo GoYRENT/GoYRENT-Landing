933013ff3dab21bbfb9e875cc6dafbeb6f7acfeb
Yurier <yurierjesus@gmail.com>
2024-07-13 10:13:53 -0400

    update

--- app/app.config.ts
+++ app/app.config.ts
@@ -2,6 +2,14 @@ export default defineAppConfig({
   ui: {
     primary: 'blue',
     gray: 'zinc',
+    variables: {
+      light: {
+        background: 'var(--color-gray-50)'
+      },
+      dark: {
+        background: 'var(--color-gray-950)'
+      }
+    },
     button: {
       rounded: 'rounded-full',
       default: {
